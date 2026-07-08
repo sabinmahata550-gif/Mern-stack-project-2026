75da32b932ca457a8341afdb07c195aed43d6c05
Full_stackAPP <sabinmahata550@gmail.com>
2026-07-08 17:29:12 +0545

    add getProductByID

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -42,6 +42,31 @@ const getAllMyProduct = async (req, res) => {
         });
     }
 };
+
+
+import Product from "../models/Product.js";
+
+export const getProductById = async(req,res)=>{
+    try{
+
+        const product = await Product.findById(req.params.id);
+
+        if(!product){
+            return res.status(404).json({
+                message:"Product not found"
+            });
+        }
+
+        res.status(200).json(product);
+
+    }catch(error){
+
+        res.status(500).json({
+            message:error.message
+        });
+
+    }
+}
 // UPDATE
 const updateMyProduct = async (req, res) => {
     try {
