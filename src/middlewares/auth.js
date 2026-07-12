18ead4febba637c3219cc9bf0c9aa0df5a546a3a
Full_stackAPP <sabinmahata550@gmail.com>
2026-07-12 14:01:35 +0545

    use token from auth header if availlabel

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,32 @@
-import { decode } from "jsonwebtoken";
 import jwt from "../utils/jwt.js";
+
 const auth = (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        const authHeader = req.headers.authorization;
+        let token;
+
+        if (authHeader && authHeader.startsWith("Bearer ")) {
+            token = authHeader.split(" ")[1];
+        } else {
+            token = req.cookies?.token;
+        }
+
         if (!token) {
             return res.status(401).json({
-                message: "User not authenticated"
+                message: "User not authenticated",
             });
         }
+
         const decoded = jwt.verifyToken(token);
         req.user = decoded.user;
-        next();
 
+        next();
     } catch (error) {
-        res.status(500).json({
-            message: "Auth failed",
-            error: error.message
+        return res.status(401).json({
+            message: "Invalid or expired token",
+            error: error.message,
         });
     }
 };
 
-export default auth
+export default auth;
