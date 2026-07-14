3a5e71e56b84e8cbde0106a64cbb59207696217c
Full_stackAPP <sabinmahata550@gmail.com>
2026-07-14 10:58:23 +0545

    gemini console

--- src/utils/ai.js
+++ src/utils/ai.js
@@ -4,11 +4,13 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 const ai = new GoogleGenAI({ apiKey: config.GEMINI_API_KEY });
 
-const promptAI = async (promptMessage) =>{
+const promptAI = async (promptMessage) => {
   const response = await ai.models.generateContent({
     model: 'gemini-2.5-flash',
     contents: promptMessage,
   });
+  console.log("Gemini Response:", response);
+
   return response.text;;
 }
 
